e8d83239e15f90f0e63b0081e3098764b106f45d
Jabari Bell <jabari.bell@pxlflu.net>
2012-09-18 12:11:26 -0400

    category table is in view with dummy items

--- Resources/ui/CategoryListViewWindow.js
+++ Resources/ui/CategoryListViewWindow.js
@@ -1,4 +1,19 @@
 exports.CategoryListViewWindow = function(args) {
 	self = Ti.UI.createWindow(args);
+	
+	var sectionFruit = Ti.UI.createTableViewSection({ headerTitle: 'Categories' });
+	sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Clothing' }));
+	sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Drink' }));
+	sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Electronics' }));
+	sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Food' }));
+	sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Video Games' }));
+	
+	var table = Ti.UI.createTableView({
+	  data: [sectionFruit]
+	});
+	table.style = Ti.UI.iPhone.TableViewStyle.GROUPED;
+	
+	self.add(table);
+	
 	return self;
 }
