b098ab5d64f16f4a9e55d128660feefa8a87b289
Jabari Bell <jabari.bell@pxlflu.net>
2012-09-21 23:15:59 -0400

    got date bottom bounds set

--- Resources/ui/AddLimitWindow.js
+++ Resources/ui/AddLimitWindow.js
@@ -1,10 +1,46 @@
 exports.AddLimitWindow = function(args) {
 	var self = Ti.UI.createWindow(args);
 	
+	var doneButton = Titanium.UI.createButton({ systemButton : Titanium.UI.iPhone.SystemButton.DONE}); 
+	doneButton.enabled = false;
+	self.setRightNavButton(doneButton);
+	
+	doneButton.addEventListener('click', function(){
+		
+	});
+	
+	var limiterDate = '';
+	var limiterType = -1;
+	
+	function evaluateFields(){
+		var isNameFieldValid = false;
+		var isLimiterDateValid = false;
+		if(limitNameTextField.value !== ''){
+			isNameFieldValid = true;
+		} else {
+			isNameFieldValid = false;
+		}
+		
+		if(limiterDate !== ''){
+			isLimiterDateValid = true;
+		} else {
+			isLimiterDateValid = false;
+		}
+
+		if(isNameFieldValid && isLimiterDateValid) {
+			doneButton.enabled = true;
+		} else {
+			doneButton.enabled = false;
+		}
+	}
+	
 	var limitNameRow = Ti.UI.createTableViewRow();
+	var date = new Date();
+	var myLimitDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 	var picker = Ti.UI.createPicker({
 		top:43,
-		type:Ti.UI.PICKER_TYPE_DATE
+		type:Ti.UI.PICKER_TYPE_DATE,
+		minDate:myLimitDate
 	});
 	var slide_in =  Titanium.UI.createAnimation({bottom:0});
 	var slide_out =  Titanium.UI.createAnimation({bottom:-251});
@@ -24,7 +60,7 @@ exports.AddLimitWindow = function(args) {
 		style:Titanium.UI.iphone.SystemButtonStyle.DONE
 	});
 	 
-	var spacer =  Titanium.UI.createButton({
+	var spacer = Titanium.UI.createButton({
 		systemButton:Titanium.UI.iphone .SystemButton.FLEXIBLE_SPACE
 	});
 	 
@@ -35,10 +71,14 @@ exports.AddLimitWindow = function(args) {
 	
 	cancel.addEventListener('click', function(){
 		picker_view.animate(slide_out);
+		evaluateFields();
 	});
 	
 	done.addEventListener('click', function(){
 		picker_view.animate(slide_out);
+		//limiterDate = picker.getValue();
+		dateRow.title = 'Limited Until: ' + limiterDate;
+		evaluateFields();
 	});
 	
 	picker_view.add(toolbar);
@@ -56,11 +96,16 @@ exports.AddLimitWindow = function(args) {
 		returnKeyType: Ti.UI.RETURNKEY_DONE
 	});
 	
+	limitNameTextField.addEventListener('change', function(){
+		evaluateFields();
+	});
+	
 	limitNameRow.add(limitNameTextField);
 	
-	var sectionCategory = Ti.UI.createTableViewSection({ headerTitle:'Limit Information' });
+	var sectionCategory = Ti.UI.createTableViewSection({ headerTitle:'Limit Information'});
 	sectionCategory.add(limitNameRow);
-	sectionCategory.add(Ti.UI.createTableViewRow({title:'Limited Until', hasChild:true}));
+	var dateRow = Ti.UI.createTableViewRow({title:'Limited Until', hasChild:true, id:0});
+	sectionCategory.add(dateRow);
 	sectionCategory.add(Ti.UI.createTableViewRow({title:'Limit Type', hasChild:true}));
 	
 	var table = Ti.UI.createTableView({
@@ -70,7 +115,8 @@ exports.AddLimitWindow = function(args) {
 	table.style = Ti.UI.iPhone.TableViewStyle.GROUPED;
 	
 	table.addEventListener('click', function(e){
-		if(e.row.title === 'Limited Until') {
+		if(e.row.id === 0) {
+			limitNameTextField.blur();
 			picker_view.animate(slide_in);
 		} else if(e.row.title == 'Limit Type') {
 			var LimitTypeListView = require('ui/LimitTypeListView').LimitTypeListView;
@@ -82,10 +128,11 @@ exports.AddLimitWindow = function(args) {
 			self.containingTab.open(limitTypeListView,{animated:true});
 		}
 	});
-	
-	picker.addEventListener('change',function(e) {
-		Ti.API.info(e.value);
-	});
+
+	 picker.addEventListener('change',function(e) {
+		// Ti.API.info(e.value);
+		 limiterDate = e.value;
+	 });
 	
 	self.add(table);
 	self.add(picker_view);
